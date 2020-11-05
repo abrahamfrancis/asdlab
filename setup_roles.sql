9185bc78661e58d9fd2279a8250f399bcb191e8b
Abraham Francis <abfr049@gmail.com>
2020-11-05 21:29:00 +0530

    give user rights to sequences

--- setup_roles.sql
+++ setup_roles.sql
@@ -17,3 +17,8 @@ grant update on player_auction.admin to web_anon;
 grant update on player_auction.player to web_anon;
 grant update on player_auction.team to web_anon;
 grant update on player_auction.auction_bids to web_anon;
+
+GRANT USAGE, SELECT ON SEQUENCE player_auction.admin_id_seq to web_anon;
+GRANT USAGE, SELECT ON SEQUENCE player_auction.team_id_seq to web_anon;
+GRANT USAGE, SELECT ON SEQUENCE player_auction.player_id_seq to web_anon;
+GRANT USAGE, SELECT ON SEQUENCE player_auction.auction_bids_id_seq to web_anon;
