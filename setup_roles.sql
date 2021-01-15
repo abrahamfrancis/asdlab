b06fcf6c6a8a3a83130df86d12db7f77666a144f
SriniPai285 <48677009+SriniPai285@users.noreply.github.com>
2021-01-15 23:24:38 +0530

    Update setup_roles.sql

--- setup_roles.sql
+++ setup_roles.sql
@@ -5,6 +5,7 @@ grant web_anon to postgres;
 grant usage on schema player_auction to web_anon;
 grant select on player_auction.admin to web_anon;
 grant select on player_auction.player to web_anon;
+
 grant select on player_auction.team to web_anon;
 grant select on player_auction.auction_bids to web_anon;
 
